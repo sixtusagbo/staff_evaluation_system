3b1fbe9a50ef87eedfffd5c3ce89c608143f8191
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-11-07 19:10:21 +0100

    Design dashboard layout

--- database/migrations/2023_11_07_174736_create_leaves_table.php
+++ database/migrations/2023_11_07_174736_create_leaves_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('leaves', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('user');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->date('start_date');
             $table->date('end_date');
             $table->string('reason');
